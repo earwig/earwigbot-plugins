b1d509540641ba429fce9ec4c360aa0a97c0ab66
Ben Kurtovic <ben.kurtovic@gmail.com>
2014-02-18 03:15:03 -0500

    Add {{reflist-talk}} to a case if it has <ref>s and no reflist.

--- tasks/drn_clerkbot.py
+++ tasks/drn_clerkbot.py
@@ -315,6 +315,7 @@ class DRNClerkBot(Task):
         elif case.status in [self.STATUS_RESOLVED, self.STATUS_CLOSED,
                              self.STATUS_FAILED]:
             self.clerk_closed_case(case, signatures)
+        self.add_missing_reflist(case)
         self.save_case_updates(conn, case, volunteers, signatures, storedsigs)
         return notices
 
@@ -510,6 +511,14 @@ class DRNClerkBot(Task):
         log = u"    {0}: will try to notify {1} parties with '{2}'"
         self.logger.debug(log.format(case.id, len(notices), template))
         return notices
+
+    def add_missing_reflist(self, case):
+        """Add {{reflist-talk}} to a case if it has <ref>s and no reflist."""
+        code = mw_parse(case.body)
+        if code.filter_tags(matches=lambda t: t.name.lower() == "ref"):
+            if any(s in case.body.lower() for s in ("reflist", "<references")):
+                return
+            case.body += "\n===References===\n{{reflist-talk|close=1}}\n"
 
     def save_case_updates(self, conn, case, volunteers, sigs, storedsigs):
         """Save any updates made to a case and signatures in the database."""
